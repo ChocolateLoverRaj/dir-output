066d33ca09da6d2b4faa636f319efc8cf183f52a
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-12-21 16:20:30 -0600

    remove and createDir

--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,14 @@
  * @description An interface for managing a dir.
  */
 import { promises as fs } from 'fs'
+import { emptyDir } from 'fs-extra'
 import { join } from 'path'
+
+export enum PreCreate {
+  DELETED = 0,
+  PRESERVED = 1,
+  CREATED = 2
+}
 
 /**
  * DirOutput class
@@ -12,6 +19,9 @@ class DirOutput {
   outputPath: string
   knowExist = new Map<string, boolean>()
   knowNoExist = new Set()
+  additionalFiles = false
+  preDelete = new Map<string, Promise<boolean>>()
+  preCreate = new Map<string, Promise<PreCreate>>()
 
   /**
    * @param outputPath The path to the dir.
@@ -41,31 +51,66 @@ class DirOutput {
   }
 
   /**
-   * Remove a sub dir. Returns true if file was deleted, and false if file did not exist.
-   * @param dir The name of the dir.
+   * Remove a file or recursively remove a dir. Returns true if file was deleted, and false if file did not exist.
+   * @param file The name of the dir.
    * @returns {Promise}
    * @fulfil boolean
    */
-  async removeDir (dir: string): Promise<boolean> {
+  async remove (file: string): Promise<boolean> {
     // Check if it doesn't exist
-    if (this.knowNoExist.has(dir)) {
+    if (this.knowNoExist.has(file)) {
       return false
     }
-    const done = () => {
-      this.knowNoExist.add(dir)
-    }
-    try {
-      await fs.rmdir(join(this.outputPath, dir))
-      done()
-      return true
-    } catch (e) {
-      if (e.code === 'ENOENT') {
-        // ENOENT is okay
-        done()
-        return false
-      } else {
-        throw e
+    // Function to remove it
+    const remove = async () => {
+      const done = () => {
+        this.knowExist.delete(file)
+        this.knowNoExist.add(file)
+        this.preDelete.delete(file)
+        this.preCreate.delete(file)
       }
+      try {
+        await fs.rm(join(this.outputPath, file), { recursive: true })
+        done()
+        return true
+      } catch (e) {
+        if (e.code === 'ENOENT') {
+          // ENOENT is okay
+          done()
+          return false
+        } else {
+          throw e
+        }
+      }
+    }
+    // Wait for preDelete function, if it exists
+    const preDelete = this.preDelete.get(file)
+    if (preDelete) {
+      return await preDelete
+    } else {
+      const preDelete = remove()
+      this.preDelete.set(file, preDelete)
+      this.preCreate.set(file, preDelete.then(() => PreCreate.DELETED))
+      return await preDelete
+    }
+  }
+
+  /**
+   * Creates a dir. If this dir was scheduled to be deleted by a `empty` operation, this dir is not deleted.
+   * @param name=true The name of the dir.
+   * @param empty Whether or not to empty the dir if it was preserved instead of deleted.
+   */
+  async createDir (name: string, empty = true): Promise<void> {
+    const dirPath = join(this.outputPath, name)
+    // Check preCreate
+    const preCreate = this.preCreate.get(name)
+    if (!preCreate || await preCreate === PreCreate.DELETED) {
+      const create = fs.mkdir(dirPath)
+      this.preCreate.set(name, create.then(() => PreCreate.CREATED))
+      await create
+      this.preCreate.delete(name)
+    } else if ((await preCreate) === PreCreate.PRESERVED && empty) {
+      await emptyDir(dirPath)
     }
   }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,159 @@
 /* eslint-env mocha */
-import DirOutput from '../lib/index'
+import DirOutput, { PreCreate } from '../lib/index'
+import mock from 'mock-fs'
 import { strictEqual } from 'assert'
+import { existsSync, mkdirSync, writeFileSync } from 'fs'
 
-it('new', () => {
-  const dirOutput = new DirOutput('path')
-  strictEqual(dirOutput.outputPath, 'path')
+beforeEach(() => {
+  mock()
+})
+
+afterEach(() => {
+  mock.restore()
+})
+
+describe('remove', () => {
+  it('file', async () => {
+    mkdirSync('path')
+    writeFileSync('path/file', '')
+    const dirOutput = new DirOutput('path')
+    strictEqual(await dirOutput.remove('file'), true)
+    strictEqual(existsSync('path/file'), false)
+  })
+
+  it('dir', async () => {
+    mkdirSync('path')
+    mkdirSync('path/dir')
+    mkdirSync('path/dir/sub-dir')
+    const dirOutput = new DirOutput('path')
+    strictEqual(await dirOutput.remove('dir'), true)
+    strictEqual(existsSync('path/dir'), false)
+  })
+
+  it('ENOENT', async () => {
+    mkdirSync('path')
+    const dirOutput = new DirOutput('path')
+    strictEqual(await dirOutput.remove('hi'), false)
+  })
+
+  it('sets knowNoExist', async () => {
+    mkdirSync('path')
+    const dirOutput = new DirOutput('path')
+    await dirOutput.remove('hi')
+    strictEqual(dirOutput.knowNoExist.has('hi'), true)
+  })
+
+  it('reads knowNoExist', async () => {
+    mkdirSync('path')
+    writeFileSync('path/file', '')
+    const dirOutput = new DirOutput('path')
+    dirOutput.knowNoExist.add('file')
+    strictEqual(await dirOutput.remove('file'), false)
+    strictEqual(existsSync('path/file'), true)
+  })
+
+  it('uses preDelete', async () => {
+    mkdirSync('path')
+    writeFileSync('path/file', '')
+    const dirOutput = new DirOutput('path')
+    dirOutput.preDelete.set('file', Promise.resolve(false))
+    strictEqual(await dirOutput.remove('file'), false)
+    strictEqual(existsSync('path/file'), true)
+  })
+
+  it('sets preDelete', async () => {
+    mkdirSync('path')
+    const dirOutput = new DirOutput('path')
+    const remove = dirOutput.remove('file')
+    strictEqual(await dirOutput.preDelete.get('file'), false)
+    await remove
+  })
+
+  it('deletes preDelete', async () => {
+    mkdirSync('path')
+    const dirOutput = new DirOutput('path')
+    await dirOutput.remove('file')
+    strictEqual(dirOutput.preDelete.has('file'), false)
+  })
+
+  it('sets preCreate', async () => {
+    mkdirSync('path')
+    const dirOutput = new DirOutput('path')
+    const remove = dirOutput.remove('file')
+    strictEqual(await dirOutput.preCreate.get('file'), PreCreate.DELETED)
+    await remove
+  })
+
+  it('deletes preCreate', async () => {
+    mkdirSync('path')
+    const dirOutput = new DirOutput('path')
+    await dirOutput.remove('file')
+    strictEqual(dirOutput.preCreate.has('file'), false)
+  })
+})
+
+describe('createDir', () => {
+  it('create', async () => {
+    mkdirSync('path')
+    const dirOutput = new DirOutput('path')
+    await dirOutput.createDir('dir')
+    strictEqual(existsSync('path/dir'), true)
+  })
+
+  describe('uses preCreate', () => {
+    it('DELETED', async () => {
+      mkdirSync('path')
+      const dirOutput = new DirOutput('path')
+      dirOutput.preCreate.set('dir', Promise.resolve(PreCreate.DELETED))
+      await dirOutput.createDir('dir')
+      strictEqual(existsSync('path/dir'), true)
+    })
+
+    describe('PRESERVED', () => {
+      it('empty', async () => {
+        mkdirSync('path')
+        mkdirSync('path/dir')
+        mkdirSync('path/dir/file')
+        const dirOutput = new DirOutput('path')
+        dirOutput.preCreate.set('dir', Promise.resolve(PreCreate.PRESERVED))
+        await dirOutput.createDir('dir')
+        strictEqual(existsSync('path/dir/file'), false)
+      })
+
+      it('not empty', async () => {
+        mkdirSync('path')
+        mkdirSync('path/dir')
+        mkdirSync('path/dir/file')
+        const dirOutput = new DirOutput('path')
+        dirOutput.preCreate.set('dir', Promise.resolve(PreCreate.PRESERVED))
+        await dirOutput.createDir('dir', false)
+        strictEqual(existsSync('path/dir/file'), true)
+      })
+    })
+
+    it('CREATED', async () => {
+      mkdirSync('path')
+      mkdirSync('path/dir')
+      mkdirSync('path/dir/file')
+      const dirOutput = new DirOutput('path')
+      dirOutput.preCreate.set('dir', Promise.resolve(PreCreate.CREATED))
+      await dirOutput.createDir('dir')
+      strictEqual(existsSync('path/dir/file'), true)
+    })
+  })
+
+  it('sets preCreate', async () => {
+    mkdirSync('path')
+    const dirOutput = new DirOutput('path')
+    const create = dirOutput.createDir('dir')
+    strictEqual(await dirOutput.preCreate.get('dir'), PreCreate.CREATED)
+    await create
+  })
+
+  it('deletes preCreate', async () => {
+    mkdirSync('path')
+    const dirOutput = new DirOutput('path')
+    await dirOutput.createDir('dir')
+    strictEqual(dirOutput.preCreate.has('dir'), false)
+  })
 })
